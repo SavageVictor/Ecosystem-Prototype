3713536ee11c2222497d749f71bf590da37b95e0
SavageVictor <victor.v.oliinyk@gmail.com>
2022-04-22 21:35:08 +0300

    Simple Draggable Camera
    todo: Zoom in/out

---
+++ Assets/Scripts/Camera/CameraDragMove.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CameraDragMove : MonoBehaviour
+{
+    private Vector3 _origin;
+    private Vector3 _difference;
+
+    private bool _isDragged = false;
+
+    [SerializeField] private Camera generalCamera;
+
+    void LateUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _difference = generalCamera.ScreenToWorldPoint(Input.mousePosition) - generalCamera.transform.position;
+            if (_isDragged == false)
+            {
+                _isDragged = true;
+                _origin = generalCamera.ScreenToWorldPoint(Input.mousePosition);
+            }
+        }
+        else
+        {
+            _isDragged = false;
+        }
+
+        if (_isDragged == true)
+        {
+            generalCamera.transform.position = _origin - _difference;
+        }
+        
+        // Debug.Log(generalCamera.ScreenToWorldPoint(Input.mousePosition));
+    }
+}
